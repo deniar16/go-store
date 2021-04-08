68d4f9497ffa4a6316ffd658ee56b7da523dc95e
Harvan Irsyadi <harvanir@gmail.com>
2021-04-08 11:18:13 +0700

    Refactor router

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"fmt"
 	productGateway "github.com/deniarianto1606/go-store/gateway/product"
 	po "github.com/deniarianto1606/go-store/order/ports"
 	pp "github.com/deniarianto1606/go-store/product/ports"
 	"github.com/deniarianto1606/go-store/product/ports/findbycode"
 	"github.com/deniarianto1606/go-store/product/ports/save"
 	"go.mongodb.org/mongo-driver/mongo"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
 )
 
 type appContext struct {
@@ -56,26 +51,5 @@ func main() {
 	initializeRepo(ctx.db, ctx.repo)
 	initializeGateway(ctx.repo, ctx.gateway)
 	initializeUseCase(ctx.gateway, ctx.useCase)
-	r := initializeRouter(ctx.useCase, ctx.repo)
-
-	errs := make(chan error, 2)
-	go func() {
-		fmt.Println("Listening on port :50001")
-		errs <- http.ListenAndServe(httpPort(), r)
-	}()
-
-	go func() {
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, syscall.SIGINT)
-		errs <- fmt.Errorf("%s", <-c)
-	}()
-	fmt.Printf("Terminated %s", <-errs)
-}
-
-func httpPort() string {
-	port := "50001"
-	if os.Getenv("PORT") != "" {
-		port = os.Getenv("PORT")
-	}
-	return fmt.Sprintf(":%s", port)
+	initializeRouter(ctx.useCase, ctx.repo)
 }
--- controller/product/common.go
+++ controller/product/common.go
@@ -2,26 +2,9 @@ package product
 
 import (
 	"encoding/json"
-	"github.com/deniarianto1606/go-store/product/ports/findbycode"
-	"github.com/deniarianto1606/go-store/product/ports/save"
 	"io/ioutil"
 	"net/http"
 )
-
-// Handler product handler
-type Handler interface {
-	FindByCode(w http.ResponseWriter, r *http.Request)
-	CreateProduct(w http.ResponseWriter, r *http.Request)
-}
-
-type handler struct {
-	findByCode findbycode.UseCase
-	save       save.UseCase
-}
-
-func NewHandler(findByCode findbycode.UseCase, save save.UseCase) Handler {
-	return &handler{findByCode: findByCode, save: save}
-}
 
 func writeResponse(res []byte, w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "application/json")
